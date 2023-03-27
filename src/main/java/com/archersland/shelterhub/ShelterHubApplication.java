8b4dba8fc11053e44a3814c8a4a5553b3cbbd130
Wagner Arcieri <wagner.arcieri@gmail.com>
2023-03-27 07:53:31 -0300

    initial commit

---
+++ src/main/java/com/archersland/shelterhub/ShelterHubApplication.java
@@ -0,0 +1,13 @@
+package com.archersland.shelterhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelterHubApplication.class, args);
+	}
+
+}
