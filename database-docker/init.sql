ed96c2b1b1d86b7dc93541455b43daf087c20ba9
Wagner Arcieri <wagner.arcieri@gmail.com>
2023-04-11 11:24:21 -0300

    refactor: added initial DB scripts

--- database-docker/init.sql
+++ database-docker/init.sql
@@ -1,1 +1,27 @@
 CREATE DATABASE postgres;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+create table animal
+(
+    id uuid default uuid_generate_v4(),
+    name text,
+    age smallint,
+    medical_record_id UUID,
+    animal_type text,
+    primary key (id)
+);
+
+create table medical_record
+(
+    id uuid default uuid_generate_v4(),
+    animal_id UUID,
+    primary key (id)
+
+);
+
+alter table animal add constraint fk_medical_record_id foreign key(medical_record_id)
+    references medical_record(id);
+
+alter table medical_record add constraint fk_animal_id foreign key(animal_id)
+    references animal(id);
